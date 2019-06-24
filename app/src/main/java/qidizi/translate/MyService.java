6927de9d72c4f95edef2a04f150865ea0d1fdbe8
qidizi <qidizi@qq.com>
2019-06-24 19:54:17 +0800

    优化

--- app/src/main/java/qidizi/translate/MyService.java
+++ app/src/main/java/qidizi/translate/MyService.java
@@ -1,8 +1,10 @@
 package qidizi.translate;
 
+import android.annotation.SuppressLint;
 import android.app.*;
 import android.content.*;
 import android.graphics.*;
+import android.net.Uri;
 import android.os.*;
 import android.provider.Settings;
 import android.util.*;
@@ -10,15 +12,20 @@ import android.view.*;
 import android.webkit.*;
 import android.widget.*;
 
-import java.net.*;
-
 public class MyService extends Service {
+    private static boolean is_show = false;
+
     @Override
     public void onCreate() {
         // TODO: Implement this method
         super.onCreate();
         //service类中，只能放到onCreate之后，context才存在
         final ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        if (null == cm) {
+            Toast.makeText(this, "无法监听剪切板", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         cm.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
             @Override
@@ -52,7 +59,14 @@ public class MyService extends Service {
         return null;
     }
 
+    // 抑制提示xss风险警告
+    @SuppressLint("setJavaScriptEnabled")
     public void showText(String text) {
+        if (is_show) {
+            // 防止出现多个浮窗
+            return;
+        }
+
         if (text.length() > 1000) {
             Toast.makeText(this, "内容太长", Toast.LENGTH_SHORT).show();
             return;
@@ -64,20 +78,28 @@ public class MyService extends Service {
                 //若没有权限，提示获取.
                 Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
                 Toast.makeText(this, "请允许本应用使用悬浮窗后再试", Toast.LENGTH_LONG).show();
+                // 显示权限操作面板
                 startActivity(intent);
                 return;
             }
+        }
+
+        is_show = true;
+        final WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+
+        if (null == wm) {
+            Toast.makeText(this, "无法获取窗口管理器（WindowManager）", Toast.LENGTH_LONG).show();
+            return;
         }
 
         int window_type = 0;
         //8.0系统加强后台管理，禁止在其他应用和窗口弹提醒弹窗，如果要弹，必须使用TYPE_APPLICATION_OVERLAY
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             window_type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-        }else {
+        } else {
             window_type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
         }
 
-        final WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics dm = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(dm);
 
@@ -91,22 +113,24 @@ public class MyService extends Service {
         lp.gravity = Gravity.CENTER_VERTICAL | Gravity.TOP;
         wm.addView(btn, lp);
 
-
         final WebView web = new WebView(this);
         web.requestFocus();
         web.setBackgroundColor(Color.TRANSPARENT);
         web.getSettings().setJavaScriptEnabled(true);
 
-        web.loadUrl("https://cn.bing.com/dict/search?q=" + URLEncoder.encode(text));
+        web.loadUrl("https://cn.bing.com/dict/search?q=" + Uri.encode(text));
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.MATCH_PARENT, dm.heightPixels / 2, 0, 0,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                dm.heightPixels / 2, 0, 0,
                 window_type,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
-                PixelFormat.TRANSLUCENT);
-        // layoutParams.setTitle("Window test");
+                PixelFormat.TRANSPARENT
+        );
         layoutParams.gravity = Gravity.TOP | Gravity.START;
+        // 背景不透明
+        layoutParams.alpha = 1.0f;
+        web.setBackgroundColor(Color.WHITE);
         wm.addView(web, layoutParams);
-
 
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -118,8 +142,7 @@ public class MyService extends Service {
                 web.clearView();
                 web.destroy();
                 wm.removeView(btn);
-
-
+                is_show = false;
             }
         });
     }
